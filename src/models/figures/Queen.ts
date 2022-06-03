2a913444cf8c9c7f047043367eba015323773c8e
Roman <roman8.dev@gmail.com>
2022-06-03 18:46:25 +0300

    ограничение на передвижение фигур

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -10,4 +10,11 @@ export class Queen extends Figure {
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.QUEEN;
     }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) {
+            return false
+        }
+        return true
+    }
 }

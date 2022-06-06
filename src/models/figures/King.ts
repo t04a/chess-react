5fb4768dd89d1dfb6bbe26e6578ae65ab1ccb92e
Roman <roman8.dev@gmail.com>
2022-06-06 16:07:00 +0300

    король логика движения

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -15,6 +15,16 @@ export class King extends Figure {
         if (!super.canMove(target)) {
             return false
         }
-        return true
+
+        const dx = Math.abs(this.cell.x - target.x)
+        const dy = Math.abs(this.cell.y - target.y)
+
+        if ((dx === 1 && dy === 1) ||
+            (dx === 0 && dy === 1) ||
+            (dx === 1 && dy === 0)) {
+            return true
+        }
+
+        return false
     }
 }
